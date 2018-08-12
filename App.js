bb2f6474e5a32b9e6b02e1b98a8180fc11999221
Louis <louis993546@gmail.com>
2018-08-12 19:59:15 +0200

    ADD: fetch timeout via RANDOM API; REFACTOR: the big switch

--- App.js
+++ App.js
@@ -31,48 +31,76 @@ export default class App extends Component<Props> {
     }
   }
 
+  onPressWhenWaiting = () => {
+    // nothing, literally, for now
+    // TODO mark this round as invalid
+  }
+
+  onPressWhenClickNow = () => {
+    //determin the new score
+    var timeNow = (new Date).getTime()
+    var diff = timeNow - this.state.timeWhenStart
+
+    //set state to restart, add a new score
+    this.setState(previousState => {
+      var newScoreBoard = previousState.scoreBoard.scores.slice(1)
+      newScoreBoard.push({score: diff})
+      return {
+        scoreBoard: { scores: newScoreBoard },
+        trafficLight: { state: 'restart' },
+        timeWhenStart: 0
+      }
+    })
+  }
+
+  onPressWhenRestart = () => {
+    console.log("clicked when restart")
+    
+    fetch('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then((response) => {
+        var text = response.text()._55
+        console.log(text)
+        return text
+      })
+      .then((seconds) => seconds * 1000)
+      .then((ms) => {
+        setTimeout(() => {
+          this.setState(previousState => {
+            return {
+              scoreBoard: previousState.scoreBoard,
+              trafficLight: {
+                state: 'clickNow'
+              },
+              timeWhenStart: (new Date).getTime()
+            };
+          });
+        }, ms);
+      })
+
+    //move back to waiting state
+    this.setState(previousState => {
+      return {
+        scoreBoard: previousState.scoreBoard,
+        trafficLight: {
+          state: 'waiting'
+        },
+        timeWhenStart: 0
+      };
+    });
+  }
+
   onPressClickMe = () => {
     console.log('button is being click, and this is the current state: ' + JSON.stringify(this.state))
     var stateEnum = this.state.trafficLight.state
     switch(stateEnum) {
       case "waiting":
-        // nothing, literally
+        this.onPressWhenWaiting()
         break;
       case "clickNow":
-        //determin the new score
-        var timeNow = (new Date).getTime()
-        var diff = timeNow - this.state.timeWhenStart
-
-        //set state to restart, add a new score
-        this.setState(previousState => {
-          var newScoreBoard = previousState.scoreBoard.scores.slice(1)
-          newScoreBoard.push({score: diff})
-          return {
-            scoreBoard: { scores: newScoreBoard },
-            trafficLight: { state: 'restart' },
-            timeWhenStart: 0
-          }
-        })
+        this.onPressWhenClickNow()
         break;
       case "restart":
-        console.log("clicked when restart")
-        setTimeout(() => {
-          this.setState(previousState => {
-            return { 
-              scoreBoard: previousState.scoreBoard, 
-              trafficLight: { state: 'clickNow' },
-              timeWhenStart: (new Date).getTime()
-            };
-          });
-        }, 2000); //TODO call API instead of hard code timeout
-        //TODO move back to waiting state
-        this.setState(previousState => {
-          return {
-            scoreBoard: previousState.scoreBoard,
-            trafficLight: { state: 'waiting' },
-            timeWhenStart: 0
-          };
-        });
+        this.onPressWhenRestart()
         break;
       default:
         throw "'" + stateEnum + "' is not a valid state"
@@ -83,7 +111,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <View>
-          <Text></Text>
+          <Text>Instructions???</Text>
         </View>
         <ScoreBoard scores={this.state.scoreBoard}/>
         <ReactionButton state={this.state.trafficLight} onPress={this.onPressClickMe}/>
